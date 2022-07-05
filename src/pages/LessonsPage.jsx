c2d026bcb7860e49a2af65688282e8c602480131
TranVietNam <vietnamtudonghoa98@gmail.com>
2022-07-05 21:07:43 +0700

    fix something

--- src/pages/LessonsPage.jsx
+++ src/pages/LessonsPage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { useParams, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const LessonsPage = () => {
-  const { lessonId } = useParams();
-
   return (
     <div>
       <h1>LessonsPage</h1>
-      <p>{lessonId}</p>
-      <Link to="/lessons/lesson1">lesson 1</Link>
+      <p>
+        <Link to="/lessons/lesson1"> lesson 1</Link>
+      </p>
     </div>
   );
 };
