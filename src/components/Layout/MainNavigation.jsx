6449fd2909c4c69ad42a8fc820621f9c55cdddee
TranVietNam <vietnamtudonghoa98@gmail.com>
2022-07-07 09:14:31 +0700

    modify navigation's style

--- src/components/Layout/MainNavigation.jsx
+++ src/components/Layout/MainNavigation.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import classes from "./MainNavigation.module.css";
 import { NavLink } from "react-router-dom";
 import userAvatar from "../../assets/img/user-avatar.jpg";
-import { MdOutlineQuiz as QuizIcon } from "react-icons/md";
+import {
+  MdOutlineQuiz as QuizIcon,
+  MdOutlinePlayLesson as LessonIcon,
+  MdOutlineKeyboardArrowRight as ArrowRightIcon,
+  MdOutlineKeyboardArrowDown as ArrowDownIcon,
+} from "react-icons/md";
 import { RiLogoutBoxLine as LoginIcon } from "react-icons/ri";
+import { BsCardHeading as FlashcardIcon } from "react-icons/bs";
 
 const MainNavigation = () => {
   return (
@@ -18,49 +24,89 @@ const MainNavigation = () => {
         </div>
         <nav>
           <ul className={classes["nav-list"]}>
-            <li>
-              <span className={classes.icon}></span>
+            <li className={classes["nav-item"]}>
               <NavLink
                 to="/lessons"
                 className={(navData) =>
                   navData.isActive ? classes.active : ""
                 }
               >
-                Lessons
+                <div className="flex-center">
+                  <LessonIcon className={classes.icon} />
+                  Lessons
+                </div>
+                <ArrowRightIcon className={classes["icon-arrow"]} />
+                <ArrowDownIcon
+                  className={`${classes["icon-arrow"]} ${classes.hidden}`}
+                />
               </NavLink>
+              <ul className={classes.subnav}>
+                <li className={classes["subnav-item"]}>
+                  <NavLink
+                    to="/lessons/beginner"
+                    className={(subnavData) =>
+                      subnavData.isActive ? classes.active : ""
+                    }
+                  >
+                    Beginner
+                  </NavLink>
+                </li>
+                <li className={classes["subnav-item"]}>
+                  <NavLink
+                    to="/lessons/intermediate"
+                    className={(subnavData) =>
+                      subnavData.isActive ? classes.active : ""
+                    }
+                  >
+                    Intermediate
+                  </NavLink>
+                </li>
+                <li className={classes["subnav-item"]}>
+                  <NavLink
+                    to="/lessons/advanced"
+                    className={(subnavData) =>
+                      subnavData.isActive ? classes.active : ""
+                    }
+                  >
+                    Advanced
+                  </NavLink>
+                </li>
+              </ul>
             </li>
-            <li>
-              <span className={classes.icon}>
-                <QuizIcon />
-              </span>
+            <li className={classes["nav-item"]}>
               <NavLink
                 to="/quiz-arena"
                 className={(navData) =>
                   navData.isActive ? classes.active : ""
                 }
               >
-                Quiz Arena
+                <div className="flex-center">
+                  <QuizIcon className={classes.icon} />
+                  Quiz Arena
+                </div>
               </NavLink>
             </li>
-            <li>
-              <span className={classes.icon}></span>
+            <li className={classes["nav-item"]}>
               <NavLink
                 to="/flashcard"
                 className={(navData) =>
                   navData.isActive ? classes.active : ""
                 }
               >
-                Flashcard
+                <div className="flex-center">
+                  <FlashcardIcon className={classes.icon} />
+                  Flashcard
+                </div>
               </NavLink>
             </li>
-            <li>
-              <LoginIcon className={classes.icon} />
+            <li className={classes["nav-item"]}>
               <NavLink
                 to="/login"
                 className={(navData) =>
                   navData.isActive ? classes.active : ""
                 }
               >
+                <LoginIcon className={classes.icon} />
                 Logout
               </NavLink>
             </li>
