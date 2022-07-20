93bfa21a610f74ebf9ea6a528b3915e421594d6f
VyTsan <tosanvy@gmail.com>
2022-07-20 15:09:26 +0700

    Thêm page Learn và Practice

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import HomePage from "./components/Layout/HomePage";
 import {
   LessonsPage, NotFoundPage, LessonsBeginnerPage, LessonsIntermediatePage, LessonsAdvancedPage, LessonDetailPage,
-  QuickExercisesPage, FlashcardPage, LoginPage, ListeningPage, ReadingPage, WritingPage, SpeakingPage
+  QuickExercisesPage, FlashcardPages, FlashcardLearnPage, FlashcardPracticePage, LoginPage, ListeningPage, ReadingPage, WritingPage, SpeakingPage
 } from "./pages";
 
 
@@ -26,7 +26,9 @@ function App() {
           <Route path="/quick-exercises/reading" element={<ReadingPage />} />
           <Route path="/quick-exercises/writing" element={<WritingPage />} />
           <Route path="/quick-exercises/speaking" element={<SpeakingPage />} />
-          <Route path="flashcard" element={<FlashcardPage />} />
+          <Route path="/flashcard/" element={<Navigate replace to="/flashcard/learn" />} />
+          <Route path="/flashcard/learn" element={<FlashcardLearnPage />} />
+          <Route path="/flashcard/practice" element={<FlashcardPracticePage />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
